2b68f6acfb6b7346a7968a9e2e97f657e0fe0d3d
Tano <tanoni44@gmail.com>
2023-08-31 22:00:53 -0300

    Finish

--- ClasesPrueba/ClasesPrueba/BankAccount.cs
+++ ClasesPrueba/ClasesPrueba/BankAccount.cs
@@ -26,14 +26,29 @@ namespace ClasesPrueba
         }
 
         private static int s_accountNumberSeed = 1234567890;
+<<<<<<< HEAD
+        private readonly decimal _minimumBalance;
+
+        public BankAccount(string name, decimal initialBalance) : this(name, initialBalance, 0) { }
+
+        public BankAccount(string name, decimal initialBalance, decimal minimumBalance)
+=======
 
         public BankAccount(string name, decimal initialBalance)
+>>>>>>> 16e7a78c32673637e43bea5219df8bcc6a8ee861
         {
             Number = s_accountNumberSeed.ToString();
             s_accountNumberSeed++;
 
+<<<<<<< HEAD
+            Owner = name;
+            _minimumBalance = minimumBalance;
+            if (initialBalance > 0)
+                MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+=======
             this.Owner = name;
             MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+>>>>>>> 16e7a78c32673637e43bea5219df8bcc6a8ee861
         }
 
         private List<Transaction> _allTransactions = new List<Transaction>();
@@ -54,12 +69,32 @@ namespace ClasesPrueba
             {
                 throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
             }
+<<<<<<< HEAD
+            Transaction? overdraftTransaction = CheckWithdrawalLimit(Balance - amount < _minimumBalance);
+            Transaction? withdrawal = new(-amount, date, note);
+            _allTransactions.Add(withdrawal);
+            if (overdraftTransaction != null)
+                _allTransactions.Add(overdraftTransaction);
+        }
+
+        protected virtual Transaction? CheckWithdrawalLimit(bool isOverdrawn)
+        {
+            if (isOverdrawn)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            else
+            {
+                return default;
+            }
+=======
             if (Balance - amount < 0)
             {
                 throw new InvalidOperationException("Not sufficient funds for this withdrawal");
             }
             var withdrawal = new Transaction(-amount, date, note);
             _allTransactions.Add(withdrawal);
+>>>>>>> 16e7a78c32673637e43bea5219df8bcc6a8ee861
         }
 
         public string GetAccountHistory()
@@ -76,5 +111,11 @@ namespace ClasesPrueba
             return report.ToString();
         }
 
+<<<<<<< HEAD
+        public virtual void PerformMonthEndTransactions() { }
     }
+
+=======
+    }
+>>>>>>> 16e7a78c32673637e43bea5219df8bcc6a8ee861
 }
--- ConsoleApp1/ConsoleApp1/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using ClasesPrueba;
-
-var account = new BankAccount("<name>", 1000);
-Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
